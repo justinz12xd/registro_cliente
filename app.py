1db69ab1d3930cb5d4c21036e8be60f9e7e04415
justinz12xd <justinalejandro996@gmail.com>
2025-05-30 09:33:35 -0500

    cambios en bd

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request, jsonify
 import sqlite3
-
+import os
+from flask_sqlalchemy import SQLAlchemy 
 app = Flask(__name__)
+
+ # Ruta absoluta a la base de datos
+basedir = os.path.abspath(os.path.dirname(__file__))
+DATABASE_PATH = os.path.join(basedir, 'db.sqlite3')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DATABASE_PATH}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
